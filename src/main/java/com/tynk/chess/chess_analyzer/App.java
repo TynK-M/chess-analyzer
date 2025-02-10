1416354877bd2c98b0d0bb1a857ce0ec0f61b770
TynK-M <164543445+TynK-M@users.noreply.github.com>
2025-02-11 00:46:39 +0100

    Initial commit

---
+++ src/main/java/com/tynk/chess/chess_analyzer/App.java
@@ -0,0 +1,13 @@
+package com.tynk.chess.chess_analyzer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
